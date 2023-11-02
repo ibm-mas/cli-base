863e6968ce362b6c37837209a1f5bd29c6dfc009
David Parker <parkerda@uk.ibm.com>
2023-11-02 13:09:08 +0000

    [major] First release of CLI base image (#1)

---
+++ build/bin/docker-build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/.env.sh
+source $DIR/.functions.sh
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -n|--namespace)
+        NAMESPACE="$2"
+        ;;
+
+        -i|--image)
+        IMAGE="$2"
+        ;;
+
+        -b|--buildpath)
+        BUILDPATH="$2"
+        ;;
+
+        -f|--file)
+        DOCKERFILE="$2"
+        ;;
+
+        *)
+        # unknown option, use as additional params directly to docker
+        EXTRA_PARAMS="$EXTRA_PARAMS $key $2"
+        ;;
+    esac
+    shift
+    shift
+done
+
+# Enable experimental features so we can use docker --squash
+cat /etc/docker/daemon.json | jq '. += {"experimental":true}' | sudo tee /etc/docker/daemon.json
+sudo systemctl restart docker.service
+
+BUILDPATH="${BUILDPATH:-image/$IMAGE}"
+DOCKERFILE="${DOCKERFILE:-$BUILDPATH/Dockerfile}"
+
+# Note: Travis will ******* out the sensitive information here, but it will prove that
+# the value matches the value in the config if it does, which is the goal!
+echo_h1 "Docker Build: $NAMESPACE/$IMAGE"
+echo "BUILDPATH ...... $BUILDPATH"
+echo "DOCKERFILE ..... $DOCKERFILE"
+echo "EXTRA_PARAMS ... $EXTRA_PARAMS"
+echo "VERSION_LABEL .. $DOCKER_TAG"
+echo "RELEASE_LABEL .. $GITHUB_RUN_ID"
+echo "VCS_REF ........ $GITHUB_SHA"
+echo "VCS_URL ........ https://github.com/$GITHUB_REPOSITORY"
+
+docker build \
+  --build-arg VERSION_LABEL=$DOCKER_TAG \
+  --build-arg RELEASE_LABEL=$GITHUB_RUN_ID \
+  --build-arg VCS_REF=$GITHUB_SHA \
+  --build-arg VCS_URL=https://github.com/$GITHUB_REPOSITORY \
+  --squash \
+  -t $NAMESPACE/$IMAGE $EXTRA_PARAMS -f $DOCKERFILE $BUILDPATH
