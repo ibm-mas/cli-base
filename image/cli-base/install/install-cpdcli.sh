9d2437f7d9a8b442351f63f1044710c464dda2eb
Andre Ricardo De Campos Marcelino <andrercm@br.ibm.com>
2024-02-03 13:15:33 -0300

    [patch] rebuild cpdcli

--- image/cli-base/install/install-cpdcli.sh
+++ image/cli-base/install/install-cpdcli.sh
@@ -5,7 +5,5 @@ set -e
 
 curl -L https://github.com/IBM/cpd-cli/releases/download/v13.1.2/cpd-cli-linux-EE-13.1.2.tgz -o cpd-cli-linux.tgz
 tar -xf cpd-cli-linux.tgz
-mv cpd-cli-linux-EE-13.1.2-89 /usr/local/bin/cpd-cli
+mv cpd-cli-linux-EE-13.1.2-89 /usr/local/bin
 rm cpd-cli-linux.tgz
-
-cpd-cli version
