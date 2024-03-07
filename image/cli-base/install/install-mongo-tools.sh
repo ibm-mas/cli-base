d0694b6329b9657f0fd8bb326a9487297dd39bec
David Parker <parkerda@uk.ibm.com>
2024-03-07 10:13:28 +0000

    Update install-mongo-tools.sh

--- image/cli-base/install/install-mongo-tools.sh
+++ image/cli-base/install/install-mongo-tools.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-# Install Mongo Shell
 set -e
 
-curl "https://downloads.mongodb.com/compass/mongodb-mongosh-shared-openssl11-1.10.5.x86_64.rpm" -o mongodb-mongosh-shared-openssl11-1.10.5.x86_64.rpm
-rpm -i mongodb-mongosh-shared-openssl11-1.10.5.x86_64.rpm
+# Install Mongo Shell
+# -----------------------------------------------------------------------------
+# https://github.com/mongodb-js/mongosh/releases
+curl "https://github.com/mongodb-js/mongosh/releases/download/v1.10.6/mongodb-mongosh-shared-openssl3-1.10.6.x86_64.rpm" -o mongodb-mongosh-shared.rpm
+rpm -i mongodb-mongosh-shared.rpm
 
 mongosh --version
-rm mongodb-mongosh-shared-openssl11-1.10.5.x86_64.rpm
+rm mongodb-mongosh-shared.rpm
+
 
 # Install Mongo Tools
-curl "https://fastdl.mongodb.org/tools/db/mongodb-database-tools-rhel80-x86_64-100.8.0.tgz" -o mongodb-database-tools-rhel80-x86_64-100.8.0.tgz
-tar xvfz mongodb-database-tools-rhel80-x86_64-100.8.0.tgz
+# -----------------------------------------------------------------------------
+# https://www.mongodb.com/try/download/database-tools
+curl "https://fastdl.mongodb.org/tools/db/mongodb-database-tools-rhel90-x86_64-100.9.4.tgz" -o mongodb-database-tools.tgz
+tar xvfz mongodb-database-tools.tgz
 
 mv mongodb-database-tools-rhel80-x86_64-100.8.0/bin/* /usr/local/bin/
 rm -rf mongodb-database-tools-rhel80-x86_64-100.8.0
-rm mongodb-database-tools-rhel80-x86_64-100.8.0.tgz
+rm mongodb-database-tools.tgz
 
 mongodump --version
