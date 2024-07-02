6dc3da3677c1dfb996a96ce5645a8f4d38ff9f57
whitfiea <whitfiea@uk.ibm.com>
2024-07-02 10:52:44 +0100

    use openssl 3

--- image/cli-base/install/install-mongo-tools.sh
+++ image/cli-base/install/install-mongo-tools.sh
@@ -3,11 +3,11 @@
 # Install Mongo Shell
 set -e
 
-curl "https://downloads.mongodb.com/compass/mongodb-mongosh-shared-openssl11-2.2.10.x86_64.rpm" -o mongodb-mongosh-shared-openssl11-2.2.10.x86_64.rpm
-rpm -i mongodb-mongosh-shared-openssl11-2.2.10.x86_64.rpm
+curl "https://downloads.mongodb.com/compass/mongodb-mongosh-shared-openssl3-2.2.9.x86_64.rpm" -o mongodb-mongosh-shared-openssl3-2.2.9.x86_64.rpm
+rpm -i mongodb-mongosh-shared-openssl3-2.2.9.x86_64.rpm
 
 mongosh --version
-rm mongodb-mongosh-shared-openssl11-2.2.10.x86_64.rpm
+rm mongodb-mongosh-shared-openssl3-2.2.9.x86_64.rpm
 
 # Install Mongo Tools
 curl "https://fastdl.mongodb.org/tools/db/mongodb-database-tools-rhel90-x86_64-100.9.5.tgz" -o mongodb-database-tools-rhel90-x86_64-100.9.5.tgz
