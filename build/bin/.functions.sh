abc929a3e9ab0215edf3ee05b9cd137900168504
shajeena <shajsyed@in.ibm.com>
2024-08-14 18:49:20 +0530

    [minor]multiarch support

--- build/bin/.functions.sh
+++ build/bin/.functions.sh
@@ -66,7 +66,10 @@ function install_buildx() {
     #https://download.docker.com/linux/rhel/8/x86_64/stable/Packages/docker-buildx-plugin-0.16.1-1.el8.x86_64.rpm
     curl --silent -L "https://github.com/docker/buildx/releases/download/v0.11.2/buildx-v0.11.2.linux-amd64" > ~/.docker/cli-plugins/docker-buildx
     chmod a+x ~/.docker/cli-plugins/docker-buildx
+
     echo "inside 1 install_buildx"
+    sudo apt-get update
+    echo "inside 1-1 install_buildx"
     sudo apt-get install -y qemu-user-static
       echo "inside2 install_buildx"
     qemu-aarch64-static --version
