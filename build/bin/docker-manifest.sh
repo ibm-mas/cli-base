ef28857402cefe22511cf0077c350556c1f9cbcb
shajeena <shajsyed@in.ibm.com>
2024-08-19 14:58:23 +0530

    [minor]multiarch support

--- build/bin/docker-manifest.sh
+++ build/bin/docker-manifest.sh
@@ -56,13 +56,13 @@ done
 FQ_IMAGE_WITH_TAG=$NAMESPACE/$IMAGE:${MANIFEST_PREFIX}${DOCKER_TAG}
 ARTIFACTORY_SRC=$ARTIFACTORY_REPO/$NAMESPACE/$IMAGE:${DOCKER_TAG}
 ARTIFACTORY_DEST=$ARTIFACTORY_REPO/$FQ_IMAGE_WITH_TAG
-echo $ARTIFACTORY_SRC
-echo $ARTIFACTORY_DEST
+echo "SRC "$ARTIFACTORY_SRC
+echo "DESC" $ARTIFACTORY_DEST
 # Publish manifest to Artifactory
 # -----------------------------------------------------------------------------
 echo_h2 "Publishing manifest to Artifactory ($TARGET_PLATFORMS)"
 MANIFEST_CMD="docker manifest create $ARTIFACTORY_DEST"
-$MANIFEST_CMD
+
 
 for TARGET_PLATFORM in $TARGET_PLATFORMS; do
   echo "Adding $TARGET_PLATFORM"
