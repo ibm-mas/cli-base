57ed6e3d51e48ee5e3187a66ab89e7c65687c251
shajeena <shajsyed@in.ibm.com>
2024-08-21 20:53:49 +0530

    [minor]Multiarch-support

--- image/cli-base/install/install-helm.sh
+++ image/cli-base/install/install-helm.sh
@@ -23,10 +23,18 @@ if [[ "$TARGET_PLATFORM" == "" ]]
   then TARGET_PLATFORM=amd64
 fi
 if [[ "$TARGET_PLATFORM" == "amd64" ]]; then
-
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-chown default:root /usr/local/bin/helm
-chmod  g=u /usr/local/bin/helm
-chmod 777 /usr/local/bin/helm
+  curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+  chmod 700 get_helm.sh
+  ./get_helm.sh
+  chown default:root /usr/local/bin/helm
+  chmod  g=u /usr/local/bin/helm
+  chmod 777 /usr/local/bin/helm
+else
+  curl -L https://get.helm.sh/helm-v3.15.3-linux-s390x.tar.gz -o helm-v3.15.3-linux-s390x.tar.gz
+  tar -xvzf helm-v3.15.3-linux-s390x.tar.gz
+  cd linux-s390x
+  mv helm /usr/local/bin/
+  chmod  g=u /usr/local/bin/helm
+  chmod 777 /usr/local/bin/helm
+  helm version
+fi
