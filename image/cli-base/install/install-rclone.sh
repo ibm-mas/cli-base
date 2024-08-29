620b549a3644be4a58a27d58815c84269bdc4d5b
shajeena <shajsyed@in.ibm.com>
2024-08-29 18:04:16 +0530

    [minor] multiarch

--- image/cli-base/install/install-rclone.sh
+++ image/cli-base/install/install-rclone.sh
@@ -1,46 +1,51 @@
-      #!/bin/bash
+        #!/bin/bash
 
-      # Install Rclone CLI
-      set -e
+        # Install Rclone CLI
+        set -e
 
-      while [[ $# -gt 0 ]]
-      do
-          key="$1"
+        while [[ $# -gt 0 ]]
+        do
+            key="$1"
 
-          case $key in
-           --target-platform)
-          TARGET_PLATFORM="$2"
-          ;;
-              *)
-              # unknown option, use as additional params directly to docker
-              EXTRA_PARAMS="$EXTRA_PARAMS $key $2"
-              ;;
-          esac
-          shift
-          shift
-      done
-
-
-      if [[ "$TARGET_PLATFORM" == "amd64" ]]; then
-        curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
-        unzip rclone-current-linux-amd64.zip
-        cp ./rclone-*-linux-amd64/rclone /usr/local/bin/
-        rclone version
-        rm -rf rclone-*
-      else
-        # doesnt have rclone-current.tar.gz
-        curl -O  https://downloads.rclone.org/v1.67.0/rclone-v1.67.0.tar.gz
-        tar -xvzf rclone-v1.67.0.tar.gz
-        cd rclone-v1.67.0
-        go build -o rclone
-        ./rclone --version
-        cp rclone /usr/bin/
-        chmod +x /usr/bin/rclone
-        rclone --version
-        cd ..
-        rm -rf rclone-*
-      fi
+            case $key in
+             --target-platform)
+            TARGET_PLATFORM="$2"
+            ;;
+                *)
+                # unknown option, use as additional params directly to docker
+                EXTRA_PARAMS="$EXTRA_PARAMS $key $2"
+                ;;
+            esac
+            shift
+            shift
+        done
 
 
-      #[root@aca72e69ab39 rclone-v1.65.2-linux-mipsle]# rclone version
-       #bash: /usr/local/bin/rclone: cannot execute binary file: Exec format error
+        if [[ "$TARGET_PLATFORM" == "amd64" ]]; then
+          curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+          unzip rclone-current-linux-amd64.zip
+          cp ./rclone-*-linux-amd64/rclone /usr/local/bin/
+          rclone version
+          rm -rf rclone-*
+        else
+          # doesnt have rclone-current.tar.gz
+          wget https://go.dev/dl/go1.21.0.linux-s390x.tar.gz
+          chmod ugo+r go1.21.0.linux-s390x.tar.gz
+          sudo tar -C /usr/local -xzf go1.21.0.linux-s390x.tar.gz
+          export PATH=/usr/local/go/bin:$PATH
+          go version
+          curl -O  https://downloads.rclone.org/v1.67.0/rclone-v1.67.0.tar.gz
+          tar -xvzf rclone-v1.67.0.tar.gz
+          cd rclone-v1.67.0
+          go build -o rclone
+          ./rclone --version
+          cp rclone /usr/bin/
+          chmod +x /usr/bin/rclone
+          rclone --version
+          cd ..
+          rm -rf rclone-*
+        fi
+
+
+        #[root@aca72e69ab39 rclone-v1.65.2-linux-mipsle]# rclone version
+         #bash: /usr/local/bin/rclone: cannot execute binary file: Exec format error
