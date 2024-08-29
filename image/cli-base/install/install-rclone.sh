2e95336327aa0eee7a6ce848650f78f9c254aac5
shajeena <shajsyed@in.ibm.com>
2024-08-29 16:15:59 +0530

    [minor] multiarch

--- image/cli-base/install/install-rclone.sh
+++ image/cli-base/install/install-rclone.sh
@@ -3,14 +3,42 @@
 # Install Rclone CLI
 set -e
 
-curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
-unzip rclone-current-linux-amd64.zip
+while [[ $# -gt 0 ]]
+do
+    key="$1"
 
-cp ./rclone-*-linux-amd64/rclone /usr/local/bin/
+    case $key in
+     --target-platform)
+    TARGET_PLATFORM="$2"
+    ;;
+        *)
+        # unknown option, use as additional params directly to docker
+        EXTRA_PARAMS="$EXTRA_PARAMS $key $2"
+        ;;
+    esac
+    shift
+    shift
+done
 
-rclone version
 
-rm -rf rclone-*
+if [[ "$TARGET_PLATFORM" == "amd64" ]]; then
+
+  curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+  unzip rclone-current-linux-amd64.zip
+
+  cp ./rclone-*-linux-amd64/rclone /usr/local/bin/
+
+  rclone version
+
+  rm -rf rclone-*
+else
+
+  curl -O  https://downloads.rclone.org/v1.58.0/rclone-v1.58.0.tar.gz
+    unzip rclone-current-linux-amd64.zip
+    cp ./rclone-*-linux-amd64/rclone /usr/local/bin/
+    rm -rf rclone-*
+fi
+
 
 
 #[root@aca72e69ab39 rclone-v1.65.2-linux-mipsle]# rclone version
