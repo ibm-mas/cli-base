b2cc8bdbf7f057a6967e1dbf012904a8785bd1f3
Anil Prajapati <169060963+anilprajapatiibm@users.noreply.github.com>
2025-06-04 17:34:46 +0530

    [patch] Update to oc latest stable build and ibm-pak 1.18.1 (#24)
    Co-authored-by: David Parker <parkerda@uk.ibm.com>

--- image/cli-base/install/install-oc.sh
+++ image/cli-base/install/install-oc.sh
@@ -7,25 +7,25 @@ do
 
   case $key in
     --target-platform)
-  TARGET_PLATFORM="$2"
-  ;;
-      *)
-      # unknown option, use as additional params directly to docker
-      EXTRA_PARAMS="$EXTRA_PARAMS $key $2"
-      ;;
+    TARGET_PLATFORM="$2"
+    ;;
+    
+    *)
+    # unknown option, use as additional params directly to docker
+    EXTRA_PARAMS="$EXTRA_PARAMS $key $2"
+    ;;
   esac
-  shift
-  shift
+  shift  # Remove key
+  shift  # Remove value
 done
 
-#fallback to amd64 if architecture not defined
+# Fallback to amd64 if architecture not defined
 if [[ "$TARGET_PLATFORM" == "" ]]
   then TARGET_PLATFORM=amd64
 fi
 
 # Install OpenShift CLI (latest stable version)
-# setting oc version to 4.17.8 due to issue: MASCORE-5777, Please validate again before April patch release
-wget -q https://mirror.openshift.com/pub/openshift-v4/$TARGET_PLATFORM/clients/ocp/4.17.8/openshift-client-linux.tar.gz
+wget -q https://mirror.openshift.com/pub/openshift-v4/$TARGET_PLATFORM/clients/ocp/stable/openshift-client-linux.tar.gz
 tar -zxf openshift-client-linux.tar.gz
 mv oc /usr/local/bin/
 mv kubectl /usr/local/bin/
@@ -35,7 +35,7 @@ echo "oc version:"
 oc version
 
 # Install oc mirror plugin (latest stable version)
-wget -q https://mirror.openshift.com/pub/openshift-v4/$TARGET_PLATFORM/clients/ocp/4.17.8/oc-mirror.tar.gz
+wget -q https://mirror.openshift.com/pub/openshift-v4/$TARGET_PLATFORM/clients/ocp/stable/oc-mirror.tar.gz
 tar -zxf oc-mirror.tar.gz
 mv oc-mirror /usr/local/bin/
 chmod +x /usr/local/bin/oc-mirror
