1eb1bdc6e75600b14abb2f0b9d19c86293d66b55
Társis Augusto <64480693+terc1997@users.noreply.github.com>
2026-08-11 11:36:19 -0300

    [minor] add new binary db2u_migrate (#38)
    Co-authored-by: David Parker <parkerda@uk.ibm.com>

---
+++ image/cli-base/install/install-db2u-migrate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Install IBM Pak oc addon
+set -e
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+      --target-platform)
+    TARGET_PLATFORM="$2"
+    ;;
+        *)
+        # unknown option, use as additional params directly to docker
+        EXTRA_PARAMS="$EXTRA_PARAMS $key $2"
+        ;;
+    esac
+    shift
+shift
+done
+#fallback to amd64 if architecture not defined
+if [[ "$TARGET_PLATFORM" == "" ]]
+  then TARGET_PLATFORM=amd64
+fi
+
+mv /tmp/install/bin/db2u_migrate-$TARGET_PLATFORM /usr/local/bin/db2u_migrate
+chmod +x /usr/local/bin/db2u_migrate
+
+echo "uname -m"
+uname -m
+
+echo "db2u_migrate --help"
+db2u_migrate --help
